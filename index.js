4a33089c459e5f12b9a568807426df292dee3fec
PAROMITA DE SARKAR <dsparo1996@gmail.com>
2024-08-08 01:26:18 +0530

    chore: added some checks

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inputContainer = document.querySelector(".inputs");
 
 inputContainer.addEventListener("keyup", (e) => {
-  if (e.key === "Backspace" || e.key === "Delete") {
+  if (isNaN(e.target.value)) {
+    e.target.value = "";
+    return;
+  } else if (e.key === "Backspace" || e.key === "Delete") {
+    e.target.value = "";
     e.target.previousElementSibling?.focus();
   } else if (e.target.value != "" && !isNaN(Number(e.target.value))) {
     e.target?.nextElementSibling?.focus();
